c289bc4d4ac08a3c79e2e3c6651e7252102d0b39
rinat <jamin@inbox.ru>
2020-03-02 19:06:59 +0300

    first version of simple video player

---
+++ app/src/main/java/com/rinatn/videoplayer/util/Resources.java
@@ -0,0 +1,29 @@
+package com.rinatn.videoplayer.util;
+
+import com.rinatn.videoplayer.models.MediaObject;
+
+public class Resources {
+
+    public static final MediaObject[] MEDIA_OBJECTS = {
+            new MediaObject("Sending Data to a New Activity with Intent Extras",
+                    "https://s3.ca-central-1.amazonaws.com/rinatn/media/VideoPlayerRecyclerView/Sending+Data+to+a+New+Activity+with+Intent+Extras.mp4",
+                    "https://s3.ca-central-1.amazonaws.com/rinatn/media/VideoPlayerRecyclerView/Sending+Data+to+a+New+Activity+with+Intent+Extras.png",
+                    "Description for media object #1"),
+            new MediaObject("REST API, Retrofit2, MVVM Course SUMMARY",
+                    "https://s3.ca-central-1.amazonaws.com/rinatn/media/VideoPlayerRecyclerView/REST+API+Retrofit+MVVM+Course+Summary.mp4",
+                    "https://s3.ca-central-1.amazonaws.com/rinatn/media/VideoPlayerRecyclerView/REST+API%2C+Retrofit2%2C+MVVM+Course+SUMMARY.png",
+                    "Description for media object #2"),
+            new MediaObject("MVVM and LiveData",
+                    "https://s3.ca-central-1.amazonaws.com/rinatn/media/VideoPlayerRecyclerView/MVVM+and+LiveData+for+youtube.mp4",
+                    "https://s3.ca-central-1.amazonaws.com/rinatn/media/VideoPlayerRecyclerView/mvvm+and+livedata.png",
+                    "Description for media object #3"),
+            new MediaObject("Swiping Views with a ViewPager",
+                    "https://s3.ca-central-1.amazonaws.com/rinatn/media/VideoPlayerRecyclerView/SwipingViewPager+Tutorial.mp4",
+                    "https://s3.ca-central-1.amazonaws.com/rinatn/media/VideoPlayerRecyclerView/Swiping+Views+with+a+ViewPager.png",
+                    "Description for media object #4"),
+            new MediaObject("Database Cache, MVVM, Retrofit, REST API demo for upcoming course",
+                    "https://s3.ca-central-1.amazonaws.com/rinatn/media/VideoPlayerRecyclerView/Rest+api+teaser+video.mp4",
+                    "https://s3.ca-central-1.amazonaws.com/rinatn/media/VideoPlayerRecyclerView/Rest+API+Integration+with+MVVM.png",
+                    "Description for media object #5"),
+    };
+}
